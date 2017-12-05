306a2bbfe5ceabbb2b5eef2637c0b63ade72f63b
Andreas Wederbrand <andreas@wederbrand.se>
2017-12-05 06:18:42 +0100

    day05 of 2017 completed

---
+++ src/main/java/advent_2017/Day05.java
@@ -0,0 +1,53 @@
+package advent_2017;
+
+public class Day05 {
+	public int part1(String input) {
+		String[] instructionsString = input.split("\\n");
+		int[] instructions = new int[instructionsString.length];
+
+		for (int i = 0, instructionsStringLength = instructionsString.length; i < instructionsStringLength; i++) {
+			String s = instructionsString[i];
+			instructions[i] = Integer.valueOf(s);
+		}
+
+		int i = 0;
+		int steps = 0;
+
+		while (i < instructions.length) {
+			steps++;
+			int j = i;
+			i += instructions[i];
+			instructions[j]++;
+		}
+
+		return steps;
+	}
+
+	public int part2(String input) {
+		String[] instructionsString = input.split("\\n");
+		int[] instructions = new int[instructionsString.length];
+
+		for (int i = 0, instructionsStringLength = instructionsString.length; i < instructionsStringLength; i++) {
+			String s = instructionsString[i];
+			instructions[i] = Integer.valueOf(s);
+		}
+
+		int i = 0;
+		int steps = 0;
+
+		while (i < instructions.length) {
+			steps++;
+			int j = i;
+			i += instructions[i];
+			if (instructions[j] >= 3) {
+				instructions[j]--;
+			}
+			else {
+				instructions[j]++;
+			}
+		}
+
+		return steps;
+	}
+}
+
