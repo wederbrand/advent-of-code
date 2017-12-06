b28517b0680e7f9130d992ffd4b5473a01c1daba
Andreas Wederbrand <andreas@wederbrand.se>
2017-12-06 06:39:09 +0100

    day06 of 2017 completed

---
+++ src/main/java/advent_2017/Day06.java
@@ -0,0 +1,86 @@
+package advent_2017;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Day06 {
+	public int part1(String input) {
+		String[] memoryString = input.split("\\t");
+		int[] memory = new int[memoryString.length];
+
+		for (int i = 0, instructionsStringLength = memoryString.length; i < instructionsStringLength; i++) {
+			String s = memoryString[i];
+			memory[i] = Integer.valueOf(s);
+		}
+
+		HashSet<String> history = new HashSet<>();
+
+		int steps = 0;
+
+		while (!history.contains(toHash(memory))) {
+			steps++;
+			history.add(toHash(memory));
+			memory = transform(memory);
+		}
+
+		return steps;
+	}
+
+	public int part2(String input) {
+		String[] memoryString = input.split("\\t");
+		int[] memory = new int[memoryString.length];
+
+		for (int i = 0, instructionsStringLength = memoryString.length; i < instructionsStringLength; i++) {
+			String s = memoryString[i];
+			memory[i] = Integer.valueOf(s);
+		}
+
+		HashMap<String, Integer> history = new HashMap<>();
+
+		int steps = 0;
+
+		while (!history.containsKey(toHash(memory))) {
+			steps++;
+			history.put(toHash(memory), steps);
+			memory = transform(memory);
+		}
+
+		return steps-history.get(toHash(memory)) + 1;
+	}
+
+	private int[] transform(int[] memory) {
+		int maxIndex = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < memory.length; i++) {
+			if (memory[i] > max) {
+				max = memory[i];
+				maxIndex = i;
+			}
+		}
+
+		int blocks = max;
+		memory[maxIndex] = 0;
+
+		while (blocks > 0) {
+			maxIndex++;
+			if (maxIndex >= memory.length) {
+				maxIndex = 0;
+			}
+			memory[maxIndex]++;
+			blocks--;
+		}
+
+		return memory;
+	}
+
+	private String toHash(int[] memory) {
+		StringBuilder hash = new StringBuilder();
+		for (int i : memory) {
+			hash.append(i).append(".");
+		}
+
+		return hash.toString();
+	}
+
+}
+
---
+++ src/test/java/advent_2017/Day06Test.java
@@ -0,0 +1,32 @@
+package advent_2017;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day06Test {
+
+	public static final String INPUT = "10\t3\t15\t10\t5\t15\t5\t15\t9\t2\t5\t8\t5\t2\t3\t6";
+
+	@Test
+	public void testPart1() throws Exception {
+		assertEquals(5, new Day06().part1("0\t2\t7\t0"));
+	}
+
+	@Test
+	public void testPart2() throws Exception {
+		assertEquals(4, new Day06().part2("0\t2\t7\t0"));
+	}
+
+	@Test
+	public void actualPart1() throws Exception {
+		System.out.println(new Day06().part1(INPUT));
+	}
+
+	@Test
+	public void actualPart2() throws Exception {
+		System.out.println(new Day06().part2(INPUT));
+	}
+
+
+}
