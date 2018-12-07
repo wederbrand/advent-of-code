ff50241d26390d74f253c8070f43654a6a165efb
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2018-12-07 22:49:54 +0100

    new version of day 6, part 1

--- go/6_1/2018_6_1.go
+++ go/6_1/2018_6_1.go
@@ -1,135 +1,91 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"regexp"
-	"os"
 	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"strconv"
 )
 
-var world [500][500]int
-var world2 [500][500]int
-var inf = make(map[int]bool)
-var BLOCKED = 999
-
-func grow(x int, y int) {
-	marker := world[x][y]
-
-	if (marker == 0) {
-		return
-	} else if (marker == BLOCKED) {
-		return
-	}
-
-	mark(x-1, y, marker)
-	mark(x+1, y, marker)
-	mark(x, y-1, marker)
-	mark(x, y+1, marker)
+type Coord struct {
+	x int
+	y int
 }
 
-func mark(x int, y int, marker int) {
-	if (x < 0) {
-		inf[marker] = true
-		return
-	}
-	if (x > len(world)-1) {
-		inf[marker] = true
-		return
-	}
-	if (y < 0) {
-		inf[marker] = true
-		return
-	}
-	if (y > len(world)-1) {
-		inf[marker] = true
-		return
-	}
-	
-	// this point can be
-	if (world2[x][y] > 0 && world2[x][y] != marker) {
-		// - marked by another, mark with BLOCKED
-		world2[x][y] = BLOCKED
-	} else if (world2[x][y] == 0 && world[x][y] == 0) {
-		// - taken by nothing, mark it	
-		world2[x][y] = marker
-	}
-}
+var world [500][500]Coord
+var coords = make([]Coord, 0)
 
-func resolve(x int, y int) {
-	// this point can be
-	if (world2[x][y] == BLOCKED) {
-		// - marked with BLOCKED, BLOCK it in world
-		world[x][y] = BLOCKED
-	} else if (world2[x][y] > 0) {
-		// - marked, transfer to world
-		world[x][y] = world2[x][y]
-	}	
+var COLLISION = Coord{-1, -1}
 
-	world2[x][y] = 0
+func closest(x, y int) Coord {
+	min := float64(2 * len(world))
+	var closest Coord
+	for _, coord := range coords {
+		dist := math.Abs(float64(coord.x-x)) + math.Abs(float64(coord.y-y))
+		if dist < min {
+			min = dist
+			closest = coord
+		} else if dist == min {
+			closest = COLLISION
+		}
+	}
+
+	return closest
 }
 
 func main() {
-	file, _ := os.Open("2018_6.input")
+	file, err := os.Open("2018_6.input");
+	if err != nil {
+		panic(err)
+	}
 	scanner := bufio.NewScanner(file)
 
 	lineMatcher := regexp.MustCompile(`^(\d+), (\d+)$`)
 
-	marker := 0
 	for scanner.Scan() {
-		marker += 1
 		lineMatch := lineMatcher.FindStringSubmatch(scanner.Text())
 		x, _ := strconv.Atoi(lineMatch[1])
 		y, _ := strconv.Atoi(lineMatch[2])
-		world[x][y] = marker
+		coords = append(coords, Coord{x, y})
 	}
 
-	for {
-		// phase 1 - write candidates to world 2
-		for x, _ := range(world) {
-			for y, _ := range(world[x]) {
-				grow(x, y)
-			}
+	for x := range world {
+		for y := range world[x] {
+			world[x][y] = closest(x, y)
 		}
-
-		// phase 1.5 - abort if none where marked
-		found := false
-		for x, _ := range(world) {
-			for y, _ := range(world[x]) {
-				if (world2[x][y] != 0) {
-					found = true
-					break
-				}				
-			}
-		}
-
-		if !found {
-			break
-		}
-	
-		// phase 2 - resolve candidates back to world
-		for x, _ := range(world) {
-			for y, _ := range(world[x]) {
-				resolve(x, y)
-			}
-		}		
 	}
 
-	// phase 3 - count
-	count := make(map[int]int)
-	max := 0
-	for x, _ := range(world) {
-		for y, _ := range(world[x]) {
-			marker := world[x][y]
-			if !inf[marker] {
-				count[marker]++
-				if count[marker] > max {
-					max = count[marker]
+	// mark the ones on the edges as useless
+	inf := make(map[Coord]bool, 0)
+
+	for x := range world {
+		for y := range world[x] {
+			if x == 0 || x == len(world)-1 || y == 0 || y == len(world[x])-1 {
+				inf[world[x][y]] = true
+			}
+		}
+	}
+
+	maxSize := 0
+	for _, coord := range coords {
+		if !inf[coord] {
+			// calculate size
+			size := 0
+			for x := range world {
+				for y := range world[x] {
+					if world[x][y] == coord {
+						size++;
+					}
 				}
 			}
-		}
-	}		
 
-	fmt.Println(inf)
-	fmt.Println(max)
+			if size > maxSize {
+				maxSize = size
+			}
+		}
+	}
+
+	fmt.Println(maxSize)
 }
