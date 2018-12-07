963ce5dfea97c6fd87ce368008d3d1a9dd31acd4
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2018-12-07 21:13:40 +0100

    day 7 done

--- go/7_1/2018_7_1.go
+++ go/7_1/2018_7_1.go
@@ -11,7 +11,7 @@ import (
 type Letters map[string]struct{}
 
 func main() {
-	file, err := os.Open("2018_7.input");
+	file, err := os.Open("2018_7.input")
 	if err != nil {
 		panic(err)
 	}
@@ -33,7 +33,7 @@ func main() {
 		}
 		graph[b][a] = struct{}{}
 	}
-	
+
 	// create a slice if keys
 	keys := make([]string, 0, len(letters))
 	for k := range letters {
@@ -42,28 +42,60 @@ func main() {
 
 	sort.Strings(keys)
 
-	result := make([]string, 0, len(letters))
+	workers := make([]int, 0)
+	workers = append(workers, 0)
+	workers = append(workers, 0)
+	workers = append(workers, 0)
+	workers = append(workers, 0)
+	workers = append(workers, 0)
 
-	outer:
+	timeFactor := 60
+	time := 0
+	events := make(map[int][]string)
+	process := make(map[string]bool)
+
 	for len(keys) > 0 {
-		for i, key := range keys {
-			if len(graph[key]) == 0 {
-				// store it
-				result = append(result, key)
-				// clean it from the others
-				for _, innerMap := range graph {
-					delete (innerMap, key)
+		for _, key := range keys {
+			if len(graph[key]) == 0 && !process[key] {
+				// ready letter
+				// take ready worker, if any
+				for i, w := range workers {
+					if w <= 0 {
+						workers[i] = timeFactor + int(key[0]) - 'A' + 1
+						events[time+workers[i]] = append(events[time+workers[i]], key)
+						process[key] = true
+						break
+					}
 				}
-				// delete it
-				keys = append(keys[:i], keys[i+1:]...)
-				// start from the beginning
-				continue outer
+
 			}
 		}
+
+		// advance time
+		time++
+		for _, event := range events[time] {
+			for _, key := range event {
+				// clean it from the others
+				for _, innerMap := range graph {
+					delete(innerMap, string(key))
+				}
+
+				// delete it
+				for i := range keys {
+					if keys[i] == string(key) {
+						keys = append(keys[:i], keys[i+1:]...)
+						break
+					}
+				}
+				process[string(key)] = false
+			}
+		}
+
+		for i := range workers {
+			workers[i]--
+		}
+
 	}
 
-	for _, r := range result {
-		fmt.Print(r)
-	}
-	fmt.Println()
+	fmt.Println(time)
 }
