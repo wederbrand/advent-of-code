49ecb5d30958a81825c3468987ef47ec42aca60c
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2018-12-09 22:54:08 +0100

    rewritten with a deque

--- go/9_1/2018_9_1.go
+++ go/9_1/2018_9_1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/tebeka/deque"
 )
 
 func main() {
@@ -18,33 +19,19 @@ func main() {
 func doIt(numberOfPlayers int, max int) int {
 	players := make(map[int]int)
 
-	balls := make([]int, 0)
-	balls = append(balls, 0)
-	lastUsedIndex := 0
+	circle := deque.New()
+	circle.Append(0)
 	for i := 1; i <= max; i++ {
-		if i % 100000 == 0 {
-			fmt.Println(i)
-		}
 		if i % 23 == 0 {
 			// collect score and pop that ball
-			score := i
-			sevenIndex := lastUsedIndex - 7
-			sevenIndex = (sevenIndex + len(balls)) % len(balls)
-			score += balls[sevenIndex]
-			balls = append(balls[:sevenIndex], balls[sevenIndex+1:]...)
-			lastUsedIndex = sevenIndex
+			circle.Rotate(7)
+			score, _ := circle.Pop()
+			circle.Rotate(-1)
 			playerIndex := i % numberOfPlayers
-			players[playerIndex] += score
+			players[playerIndex] += i + score.(int)
 		} else {
-			nextIndex := lastUsedIndex + 2
-			if lastUsedIndex == len(balls) - 1 {
-				// last position used
-				nextIndex = 1
-			}
-			balls = append(balls, 0)
-			copy(balls[nextIndex+1:], balls[nextIndex:])
-			balls[nextIndex] = i
-			lastUsedIndex = nextIndex
+			circle.Rotate(-1)
+			circle.Append(i)
 		}
 	}
 
