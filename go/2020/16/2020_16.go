6a2c8ac76e86c9e0153e476ee63ca31338f0fa05
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-16 18:37:25 +0100

    day 16 - ugly christmas sweeter

--- go/2020/16/2020_16.go
+++ go/2020/16/2020_16.go
@@ -10,11 +10,16 @@ import (
 )
 
 type rule struct {
-	name string
-	min1 int
-	max1 int
-	min2 int
-	max2 int
+	name     string
+	min1     int
+	max1     int
+	min2     int
+	max2     int
+	position int
+}
+
+func (r rule) valid(atoi int) bool {
+	return atoi >= r.min1 && atoi <= r.max1 || atoi >= r.min2 && atoi <= r.max2
 }
 
 func main() {
@@ -43,7 +48,7 @@ func main() {
 		min2, _ := strconv.Atoi(submatch[4])
 		max2, _ := strconv.Atoi(submatch[5])
 
-		r := rule{submatch[1], min1, max1, min2, max2}
+		r := rule{submatch[1], min1, max1, min2, max2, 0}
 		rules = append(rules, r)
 	}
 
@@ -59,27 +64,73 @@ func main() {
 
 	// other tickets
 	var invalid []int
+	var valids []string
+SUPEROUTER:
 	for ; i < len(input); i++ {
 		s := input[i]
 		split := strings.Split(s, ",")
 	OUTER:
 		for _, val := range split {
 			atoi, _ := strconv.Atoi(val)
-			// check val against all valid rules
+			// check val against all valids rules
 			for _, r := range rules {
-				if atoi >= r.min1 && atoi <= r.max1 || atoi >= r.min2 && atoi <= r.max2 {
+				if r.valid(atoi) {
 					// ok
 					continue OUTER
 				}
 			}
 			invalid = append(invalid, atoi)
+			continue SUPEROUTER
 		}
+		valids = append(valids, s)
 	}
 
-	sum := 0
-	for _, i := range invalid {
-		sum += i
+	// analyze all the valid ones
+	product := 1
+	completedFields := make([]int, 0)
+OHNO:
+	for fieldIndex := 0; fieldIndex < len(myTicket); fieldIndex++ {
+		for _, field := range completedFields {
+			if field == fieldIndex {
+				fmt.Println("already found")
+				continue OHNO
+			}
+		}
+
+		candidates := append([]rule{}, rules...)
+		for _, valid := range valids {
+			value := strings.Split(valid, ",")[fieldIndex]
+			atoi, _ := strconv.Atoi(value)
+			nextCandidates := candidates[:0]
+			for _, candidate := range candidates {
+				if candidate.valid(atoi) {
+					nextCandidates = append(nextCandidates, candidate)
+				}
+
+			}
+			candidates = nextCandidates
+		}
+		// should be exactly one candidate
+		if len(candidates) > 1 {
+			fmt.Println("restarting")
+			continue OHNO
+		}
+
+		completedFields = append(completedFields, fieldIndex)
+
+		newRules := rules[:0]
+		for _, r := range rules {
+			if r != candidates[0] {
+				newRules = append(newRules, r)
+			}
+		}
+		rules = newRules
+
+		if strings.HasPrefix(candidates[0].name, "departure") {
+			product *= myTicket[fieldIndex]
+		}
+		fieldIndex = -1
 	}
 
-	fmt.Println(sum)
+	fmt.Println(product)
 }
