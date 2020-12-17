2bf162c3ed504a0a6181e919a31600a8ceef09f1
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-17 08:54:21 +0100

    day 17 - done

--- go/2020/17/2020_17.go
+++ go/2020/17/2020_17.go
@@ -16,29 +16,28 @@ func main() {
 	input := strings.Split(strings.TrimSpace(string(readFile)), "\n")
 
 	offset := 7
-	var world [20][20][20]bool
+	var world [20][20][20][20]bool
 
 	for y, s := range input {
 		for x, r := range s {
 			if r == '#' {
-				world[x+offset][y+offset][offset] = true
+				world[x+offset][y+offset][offset][offset] = true
 			}
 		}
 	}
 
-	printWorld(world)
 	for i := 0; i < 6; i++ {
 		world = expand(world)
-		fmt.Println("one iteration", "")
-		printWorld(world)
 	}
 
 	cnt := 0
 	for x := 0; x < len(world); x++ {
 		for y := 0; y < len(world); y++ {
 			for z := 0; z < len(world); z++ {
-				if world[x][y][z] {
-					cnt++
+				for w := 0; w < len(world); w++ {
+					if world[x][y][z][w] {
+						cnt++
+					}
 				}
 			}
 		}
@@ -47,43 +46,46 @@ func main() {
 	fmt.Println(cnt)
 }
 
-func printWorld(world [20][20][20]bool) {
+func printWorld(world [20][20][20][20]bool) {
 	for x := 0; x < len(world); x++ {
 		for y := 0; y < len(world); y++ {
 			for z := 0; z < len(world); z++ {
-				if world[x][y][z] {
-					fmt.Println(x, y, z)
+				for w := 0; w < len(world); w++ {
+					if world[x][y][z][w] {
+						fmt.Println(x, y, z, w)
+					}
 				}
 			}
 		}
 	}
 }
 
-func expand(world [20][20][20]bool) [20][20][20]bool {
-	var newWorld [20][20][20]bool
-	for z := 0; z < len(newWorld); z++ {
-		for y := 0; y < len(newWorld); y++ {
-			for x := 0; x < len(newWorld); x++ {
-				cnt := count(world, x, y, z)
-				if world[x][y][z] {
-					// rules for when it's active
-					if cnt == 2 || cnt == 3 {
-						newWorld[x][y][z] = true
-					}
-				} else {
-					// rules for when it's inactive
-					if cnt == 3 {
-						newWorld[x][y][z] = true
+func expand(world [20][20][20][20]bool) [20][20][20][20]bool {
+	var newWorld [20][20][20][20]bool
+	for w := 0; w < len(newWorld); w++ {
+		for z := 0; z < len(newWorld); z++ {
+			for y := 0; y < len(newWorld); y++ {
+				for x := 0; x < len(newWorld); x++ {
+					cnt := count(world, x, y, z, w)
+					if world[x][y][z][w] {
+						// rules for when it's active
+						if cnt == 2 || cnt == 3 {
+							newWorld[x][y][z][w] = true
+						}
+					} else {
+						// rules for when it's inactive
+						if cnt == 3 {
+							newWorld[x][y][z][w] = true
+						}
 					}
 				}
 			}
 		}
 	}
-
 	return newWorld
 }
 
-func count(world [20][20][20]bool, x int, y int, z int) int {
+func count(world [20][20][20][20]bool, x int, y int, z int, w int) int {
 	cnt := 0
 	for xx := x - 1; xx <= x+1; xx++ {
 		if xx < 0 || xx >= len(world) {
@@ -97,11 +99,16 @@ func count(world [20][20][20]bool, x int, y int, z int) int {
 				if zz < 0 || zz >= len(world) {
 					continue
 				}
-				if xx == x && yy == y && zz == z {
-					continue
-				}
-				if world[xx][yy][zz] {
-					cnt++
+				for ww := w - 1; ww <= w+1; ww++ {
+					if ww < 0 || ww >= len(world) {
+						continue
+					}
+					if xx == x && yy == y && zz == z && ww == w {
+						continue
+					}
+					if world[xx][yy][zz][ww] {
+						cnt++
+					}
 				}
 			}
 		}
