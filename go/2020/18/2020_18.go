f26e961f98b18290be5487f166e0fc9a131967b7
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-18 07:35:43 +0100

    day 18 - partly

---
+++ go/2020/18/2020_18.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	readFile, err := ioutil.ReadFile("18/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input := strings.Split(strings.TrimSpace(string(readFile)), "\n")
+	sum := 0
+	for _, s := range input {
+		sum += solveOne(s)
+	}
+
+	fmt.Println(sum)
+}
+
+func solveOne(input string) int {
+	result := 0
+
+	pFinder := regexp.MustCompile("\\(([0-9 +*])+\\)")
+	for pFinder.MatchString(input) {
+		input = pFinder.ReplaceAllStringFunc(input, func(s string) string {
+			innerValue := solveOne(s[1 : len(s)-1])
+			itoa := strconv.Itoa(innerValue)
+			return itoa
+		})
+	}
+
+	split := strings.Split(input, " ")
+	nextIsAddition := true
+	for _, s := range split {
+		if s == "+" {
+			nextIsAddition = true
+		} else if s == "*" {
+			nextIsAddition = false
+		} else {
+			// number
+			atoi, _ := strconv.Atoi(s)
+			if nextIsAddition {
+				result += atoi
+			} else {
+				result *= atoi
+			}
+		}
+	}
+
+	return result
+}
