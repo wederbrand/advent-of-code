4533a07738f7fccfc93b5c7a29828d4f0e1c607e
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-19 13:54:22 +0100

    day 19 - done

--- go/2020/19/2020_19.go
+++ go/2020/19/2020_19.go
@@ -68,15 +68,57 @@ func main() {
 	}
 
 	// now let's check the rest of the input
-
+	// for part 2 we can "manually" check combinations of 42 and 31
 	cnt := 0
 	for _, check := range inputImages {
-		for _, rule := range rules[0] {
-			if rule == check {
-				cnt++
-				break
+		ok, part1, part2 := testOne(check, rules[42], rules[31])
+		if ok && part1 > 1 && part1 > part2 && part2 > 0 {
+			cnt++
+			continue
+		}
+	}
+
+	fmt.Println(cnt)
+}
+
+func testOne(check string, first []string, second []string) (bool, int, int) {
+	// first part
+	nbrUsed := 0
+	for _, s := range first {
+		if strings.HasPrefix(check, s) {
+			ok, part1, part2 := testOne(check[len(s):], first, second)
+			nbrUsed += 1 + part1
+			if ok {
+				return true, nbrUsed, part2
 			}
 		}
 	}
-	fmt.Println(cnt)
+
+	ok, part2 := testTwo(check, second)
+	if ok {
+		return true, nbrUsed, part2
+	}
+
+	return false, 0, 0
+}
+
+func testTwo(check string, second []string) (bool, int) {
+	if check == "" {
+		// all parts consumed
+		return true, 0
+	}
+
+	// and second part
+	nbrUsed := 0
+	for _, s := range second {
+		if strings.HasPrefix(check, s) {
+			ok, part2 := testTwo(check[len(s):], second)
+			nbrUsed += 1 + part2
+			if ok {
+				return true, nbrUsed
+			}
+		}
+	}
+
+	return false, 0
 }
