1142f9f9909be4d70dbcf2f069b5bfbea046ef84
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-20 22:38:46 +0100

    day 20 - trying something else

--- go/2020/20/2020_20.go
+++ go/2020/20/2020_20.go
@@ -20,6 +20,7 @@ type tile struct {
 	id    int
 	dot   map[pos]bool
 	sides [4]*tile // 0 up, 1 right, 2 down, 3 left
+	flippedSides [4]bool
 }
 
 func (t tile) getSide(side int, reverse bool) string {
@@ -71,8 +72,9 @@ func (t tile) getSide(side int, reverse bool) string {
 	return result
 }
 
-var edge = tile{
-	id: 0,
+func (t tile) getMapPart(dir1 int, dir2 int, offset int, lineIndex int) string {
+	// return the 8 characters with this dir1/dir2 and offset, and index (1 -> 8)
+	return ""
 }
 
 func newTile(input string) *tile {
@@ -123,6 +125,7 @@ func main() {
 	}
 
 	prod := 1
+	corners := make([]*tile, 0)
 	for _, t := range tiles {
 		sides := 0
 		for j := 0; j < 4; j++ {
@@ -131,11 +134,85 @@ func main() {
 			}
 		}
 		if sides == 2 {
+			corners = append(corners, t)
 			prod *= t.id
 		}
 	}
 
-	fmt.Println(prod)
+	fmt.Println("part1", prod)
+
+	// PART 2
+	// start on all the four corners, flipping each twice
+	middleRE := regexp.MustCompile("^(.*)#....##....##....###(.*)$")
+	for _, corner := range corners {
+		for i := 0; i < 2; i++ {
+			giantMap := makeGiantMap(corner, i==1)
+			for i := 1; i < len(giantMap)-2; i++ {
+				s := giantMap[i]
+				// look for the middle line
+				if middleRE.MatchString(s) {
+					fmt.Println("found")
+					// if found, check the -1 and +1 once
+					// if found, replace all 3 with 0
+					// count #
+					// print
+					// exit
+				}
+			}
+		}
+	}
+	fmt.Println("result")
+}
+
+// should return 1 map, starting with this tile,
+// in either it's first or second direction
+func makeGiantMap(corner *tile, flip bool) []string {
+	// result := make([]string, 0)
+	dir1 := -1
+	dir2 := -1
+
+	for dir := 0; dir < 3; dir++ {
+		nextDir := dir + 1
+		if corner.sides[dir] != nil && corner.sides[nextDir % 4] != nil {
+			// found the two sides
+			dir1 = dir
+			dir2 = nextDir
+			break
+		}
+	}
+
+	// calc what direction is RIGHT and what is DOWN. Note the flipping part.
+
+	right := dir1
+	down := dir2
+
+	if flip {
+		right, down = down, right
+	}
+
+	right %= 4
+	down %= 4
+
+	// call something recursive with tile and right direction.
+	// then, go down, find tile underneath, turn it and call the recursive part with that one, and it's right direction!
+    someThingRecursive(corner, right, flip)
+
+	return nil
+}
+
+func someThingRecursive(t *tile, right int, flip bool) {
+	// todo: collect the lines for this tile
+	// t.getMapPart(dir1, dir2, flip, 1) // 1 -> 8
+	// then move on
+	nextTile := t.sides[right]
+	if nextTile == nil {
+		// we're done
+		// TODO: return what we've got
+	} else {
+		nextTileRight := (right + 4) % 4
+		if nextTile.sides[]
+		someThingRecursive(nextTile, nextTileRight, flip)
+	}
 }
 
 func matchTiles(t1 *tile, t2 *tile) {
@@ -151,8 +228,15 @@ func matchSides(t1 *tile, t2 *tile, i int, j int) {
 	side2 := t2.getSide(j, false)
 	side2flipped := t2.getSide(j, true)
 
-	if side1 == side2 || side1 == side2flipped {
+	if side1 == side2 {
 		t1.sides[i] = t2
 		t2.sides[j] = t1
+	}
+
+	if side1 == side2flipped {
+		t1.sides[i] = t2
+		t1.flippedSides[i] = true
+		t2.sides[j] = t1
+		t2.flippedSides[j] = true
 	}
 }
