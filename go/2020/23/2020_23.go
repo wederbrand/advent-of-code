05865611a28c89436044cace39546834b2bd3361
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-23 09:31:39 +0100

    day 23 - new, with lists

--- go/2020/23/2020_23.go
+++ go/2020/23/2020_23.go
@@ -8,6 +8,11 @@ import (
 	"strings"
 )
 
+type cup struct {
+	value int
+	next  *cup
+}
+
 func main() {
 	readFile, err := ioutil.ReadFile("23/input.txt")
 	if err != nil {
@@ -15,70 +20,73 @@ func main() {
 	}
 
 	input := strings.Split(strings.TrimSpace(string(readFile)), "")
-	var ring [9]int
-	for i, s := range input {
+	prev := new(cup)
+	current := prev
+	one := prev
+	max := 0
+	for _, s := range input {
 		atoi, _ := strconv.Atoi(s)
-		ring[i] = atoi
-	}
+		if atoi > max {
+			max = atoi
+		}
+		nextCup := new(cup)
+		nextCup.value = atoi
+		prev.next = nextCup
+		prev = nextCup
 
-	for i := 0; i < 100; i++ {
-		ring = rotate(ring)
-		fmt.Println(i+1, ring)
-	}
-
-	// collect cups after 1
-	result := ""
-	oneIndex := 0
-	for i, i2 := range ring {
-		if i2 == 1 {
-			oneIndex = i
-			break
+		if atoi == 1 {
+			one = nextCup
 		}
 	}
-	for i := oneIndex + 1; i < len(ring); i++ {
-		result += strconv.Itoa(ring[i])
+
+	prev.next = current.next
+	current = current.next
+
+	for i := 0; i < 100; i++ {
+		current = rotate(current, max)
 	}
 
-	for i := 0; i < oneIndex; i++ {
-		result += strconv.Itoa(ring[i])
+	result := ""
+	for i := 0; i < 8; i++ {
+		one = one.next
+		result += strconv.Itoa(one.value)
 	}
 
 	fmt.Println(result)
-
 }
 
-func rotate(in [9]int) [9]int {
-	out := make([]int, 0)
-	lift := in[1:4]
-	rest := append(in[4:], in[0])
-	insertTarget := in[0] - 1
+func rotate(current *cup, max int) *cup {
+	lift := current.next                       // don't care for how many it is, it's all
+	current.next = current.next.next.next.next // shortcut 3 of them
 
-	for insertTarget == lift[0] || insertTarget == lift[1] || insertTarget == lift[2] {
-		insertTarget--
+	insertTarget := current.value - 1
+
+	for {
+		for insertTarget == lift.value || insertTarget == lift.next.value || insertTarget == lift.next.next.value {
+			insertTarget--
+			if insertTarget == 0 {
+				insertTarget = max
+			}
+		}
+
 		if insertTarget == 0 {
-			insertTarget = 9
+			insertTarget = max
+			continue
+		}
+
+		break
+	}
+
+	// insert the lifted ones
+	insertCup := current
+	for {
+		insertCup = insertCup.next
+		if insertCup.value == insertTarget {
+			break
 		}
 	}
 
-	if insertTarget == 0 {
-		insertTarget = 9
-	}
+	insertCup.next, lift.next.next.next = lift, insertCup.next
 
-	for insertTarget == lift[0] || insertTarget == lift[1] || insertTarget == lift[2] {
-		insertTarget--
-		if insertTarget == 0 {
-			insertTarget = 9
-		}
-	}
-
-	for _, cup := range rest {
-		out = append(out, cup)
-		if cup == insertTarget {
-			out = append(out, lift...)
-		}
-	}
-
-	var output [9]int
-	copy(output[:], out)
-	return output
+	return current.next
 }
