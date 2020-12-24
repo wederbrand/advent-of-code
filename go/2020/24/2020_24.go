a851bd923670a26be56839aba46fb4998fdf739d
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-12-24 09:15:35 +0100

    day 24 - done

--- go/2020/24/2020_24.go
+++ go/2020/24/2020_24.go
@@ -12,6 +12,11 @@ type pos struct {
 	y int
 }
 
+var minX int
+var maxX int
+var minY int
+var maxY int
+
 func (p *pos) move(dir string) {
 	switch dir {
 	case "ne":
@@ -42,7 +47,6 @@ func main() {
 	for _, inputTile := range input {
 		p := new(pos)
 		for i := 0; i < len(inputTile); i++ {
-			// 			fmt.Println(i, p, inputTile[i:])
 			r := inputTile[i]
 			switch r {
 			case 'n':
@@ -69,16 +73,91 @@ func main() {
 				p.move("w")
 			}
 		}
-		floor[*p] = !floor[*p]
+		flip(p, floor)
 		fmt.Println(p, floor[*p])
 	}
 
+	cnt := countBlackOnes(floor)
+	fmt.Println("part 1:", cnt)
+
+	for i := 0; i < 100; i++ {
+		newFloor := make(map[pos]bool)
+		for y := minY - 2; y < maxY+2; y++ {
+			for x := minX - 2; x < maxX+2; x++ {
+				p := pos{x, y}
+				cnt := countNeighbours(p, floor)
+				if floor[p] {
+					// black tile
+					if cnt == 0 || cnt > 2 {
+						// black -> white
+					} else {
+						// black -> black
+						flip(&p, newFloor)
+					}
+
+				} else {
+					// white tile
+					if cnt == 2 {
+						// white -> black
+						flip(&p, newFloor)
+					} else {
+						// white -> white
+					}
+				}
+			}
+		}
+		floor = newFloor
+		blackOnes := countBlackOnes(floor)
+		fmt.Println(i+1, blackOnes)
+	}
+}
+
+func countNeighbours(p pos, floor map[pos]bool) int {
+	cnt := 0
+	if floor[pos{p.x, p.y - 1}] {
+		cnt++
+	}
+	if floor[pos{p.x + 1, p.y}] {
+		cnt++
+	}
+	if floor[pos{p.x + 1, p.y + 1}] {
+		cnt++
+	}
+	if floor[pos{p.x, p.y + 1}] {
+		cnt++
+	}
+	if floor[pos{p.x - 1, p.y}] {
+		cnt++
+	}
+	if floor[pos{p.x - 1, p.y - 1}] {
+		cnt++
+	}
+
+	return cnt
+}
+
+func flip(p *pos, floor map[pos]bool) {
+	if p.x < minX {
+		minX = p.x
+	}
+	if p.x > maxX {
+		maxX = p.x
+	}
+	if p.y < minY {
+		minY = p.y
+	}
+	if p.y > maxY {
+		maxY = p.y
+	}
+	floor[*p] = !floor[*p]
+}
+
+func countBlackOnes(floor map[pos]bool) int {
 	cnt := 0
 	for _, black := range floor {
 		if black {
 			cnt++
 		}
 	}
-
-	fmt.Println(cnt)
+	return cnt
 }
