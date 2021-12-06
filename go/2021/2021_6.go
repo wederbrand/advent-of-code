2a6293ee5302d914f8c90c2b075f869dc6321529
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2021-12-06 07:06:35 +0100

    2021_6
    I knew each fish shouldn't be handled individually, part 2 was super fast

---
+++ go/2021/2021_6.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	readFile, err := os.ReadFile("2021/2021_6.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inFile := strings.Split(strings.TrimSpace(string(readFile)), ",")
+
+	var fish [9]int
+	for _, text := range inFile {
+		atoi, _ := strconv.Atoi(text)
+		fish[atoi]++
+	}
+
+	for i := 0; i < 256; i++ {
+		var newFish [9]int
+		for age, num := range fish {
+			if age == 0 {
+				newFish[8] += num
+				newFish[6] += num
+			} else {
+				newFish[age-1] += num
+			}
+		}
+		fish = newFish
+
+		if i == 79 || i == 255 {
+			total := 0
+			for _, i := range fish {
+				total += i
+			}
+
+			fmt.Println("after", i+1, "days", total)
+		}
+	}
+
+	os.Exit(0)
+}
