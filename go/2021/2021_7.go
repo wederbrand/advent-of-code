1218917023c30bc16b36f8d2064d9986785f892d
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2021-12-07 07:51:08 +0100

    2021_7
    boring, just plain brute force, nothing fancy

---
+++ go/2021/2021_7.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	readFile, err := os.ReadFile("2021/2021_7.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inFile := strings.Split(strings.TrimSpace(string(readFile)), ",")
+
+	crabs := make([]int, len(inFile))
+	min := math.MaxInt
+	max := math.MinInt
+	for i, text := range inFile {
+		atoi, _ := strconv.Atoi(text)
+		crabs[i] = atoi
+		if atoi < min {
+			min = atoi
+		}
+		if atoi > max {
+			max = atoi
+		}
+	}
+
+	minPrice := math.MaxInt
+	for i := min; i < max; i++ {
+		price := 0
+		for _, crab := range crabs {
+			distToMove := crab - i
+			if distToMove < 0 {
+				distToMove *= -1
+			}
+
+			crabPrice := 0
+			for j := 1; j <= distToMove; j++ {
+				crabPrice += j
+			}
+			price += crabPrice
+		}
+
+		if price < minPrice {
+			minPrice = price
+		}
+	}
+
+	fmt.Println(minPrice)
+
+	os.Exit(0)
+}
