bdaa488e5ee3ffc40179b5c7ab5794aaff2a2d06
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2022-12-01 06:52:10 +0100

    2022_01

---
+++ go/2022/01/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	readFile, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inFile := strings.Split(strings.TrimSpace(string(readFile)), "\n")
+
+	current := 0
+	m := make([]int, 0)
+	for _, s := range inFile {
+		if s == "" {
+			m = append(m, current)
+			current = 0
+		}
+		atoi, _ := strconv.Atoi(s)
+		current += atoi
+	}
+	m = append(m, current)
+
+	sort.Sort(sort.Reverse(sort.IntSlice(m)))
+
+	fmt.Println("part1: ", m[0])
+	fmt.Println("part2: ", m[0]+m[1]+m[2])
+}
