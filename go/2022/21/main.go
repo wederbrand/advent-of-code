7287ceef3b57074cf8229141c5cc947ec9d38cd5
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2022-12-21 23:19:10 +0100

    2022_21 part 2

--- go/2022/21/main.go
+++ go/2022/21/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -16,6 +17,7 @@ type monkey struct {
 	expression string
 	one        string
 	two        string
+	you        bool
 }
 
 func newMonkey(input string) *monkey {
@@ -32,10 +34,19 @@ func newMonkey(input string) *monkey {
 	return &m
 }
 
-func (m *monkey) getValue(monkeys map[string]*monkey) int {
+func (m *monkey) getValue(monkeys map[string]*monkey) (int, error) {
+	if m.you {
+		return 0, errors.New("it's me")
+	}
 	if !m.done {
-		v1 := monkeys[m.one].getValue(monkeys)
-		v2 := monkeys[m.two].getValue(monkeys)
+		v1, err1 := monkeys[m.one].getValue(monkeys)
+		if err1 != nil {
+			return 0, err1
+		}
+		v2, err2 := monkeys[m.two].getValue(monkeys)
+		if err2 != nil {
+			return 0, err2
+		}
 		switch m.expression {
 		case "+":
 			m.value = v1 + v2
@@ -51,7 +62,68 @@ func (m *monkey) getValue(monkeys map[string]*monkey) int {
 	}
 
 	m.done = true
-	return m.value
+	return m.value, nil
+}
+
+func (m *monkey) setValue(v int, monkeys map[string]*monkey) {
+	if m.you {
+		m.value = v
+		m.done = true
+		return
+	}
+
+	m1 := monkeys[m.one]
+	m2 := monkeys[m.two]
+	v1, err1 := m1.getValue(monkeys)
+	v2, err2 := m2.getValue(monkeys)
+
+	switch m.expression {
+	case "+":
+		if err1 != nil {
+			m1.setValue(v-v2, monkeys)
+		}
+		if err2 != nil {
+			m2.setValue(v-v1, monkeys)
+		}
+	case "-":
+		if err1 != nil {
+			m1.setValue(v+v2, monkeys)
+		}
+		if err2 != nil {
+			m2.setValue(v1-v, monkeys)
+		}
+	case "*":
+		if err1 != nil {
+			m1.setValue(v/v2, monkeys)
+		}
+		if err2 != nil {
+			m2.setValue(v/v1, monkeys)
+		}
+	case "/":
+		if err1 != nil {
+			m1.setValue(v*v2, monkeys)
+		}
+		if err2 != nil {
+			m2.setValue(v*v1, monkeys)
+		}
+	default:
+		panic("unknown expression")
+
+	}
+}
+
+func (m *monkey) setEquals(monkeys map[string]*monkey) {
+	m1 := monkeys[m.one]
+	m2 := monkeys[m.two]
+	v1, err1 := m1.getValue(monkeys)
+	v2, err2 := m2.getValue(monkeys)
+
+	if err1 != nil {
+		m1.setValue(v2, monkeys)
+	}
+	if err2 != nil {
+		m2.setValue(v1, monkeys)
+	}
 }
 
 func main() {
@@ -67,5 +139,9 @@ func main() {
 		m := newMonkey(s)
 		monkeys[m.name] = m
 	}
-	fmt.Println("part1:", monkeys["root"].getValue(monkeys), "in", time.Since(start))
+
+	monkeys["humn"].you = true
+	monkeys["root"].setEquals(monkeys)
+
+	fmt.Println("part2:", monkeys["humn"].value, "in", time.Since(start))
 }
