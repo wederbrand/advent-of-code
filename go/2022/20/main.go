c6bf9959e670c739a0b16a2611c88866012191a6
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2022-12-21 22:29:02 +0100

    2022_20 part 2
    my solution for part 1 worked but was slow.
    Ring.Move() is stepwise and does't do modulus internally so that was moved out.
    Only bug was doing modulo with the entire collection, not with one element removed.

--- go/2022/20/main.go
+++ go/2022/20/main.go
@@ -25,7 +25,7 @@ func main() {
 	for i, s := range inFile {
 		atoi, _ := strconv.Atoi(s)
 		r2 := ring.New(1)
-		r2.Value = atoi
+		r2.Value = atoi * 811589153
 		lookup[i] = r2
 		if atoi == 0 {
 			zero = r2
@@ -37,22 +37,26 @@ func main() {
 	}
 
 	// rotate
-	for i := 0; i < len(lookup); i++ {
-		src := lookup[i]
-		if src.Value.(int) == 0 {
-			continue
+	for times := 0; times < 10; times++ {
+		for i := 0; i < len(lookup); i++ {
+			src := lookup[i]
+			if src.Value.(int) == 0 {
+				continue
+			}
+			prev := src.Prev()
+			unlink := prev.Unlink(1)
+			n := src.Value.(int)
+			n %= len(lookup) - 1
+			dst := prev.Move(n)
+			dst.Link(unlink)
 		}
-		prev := src.Prev()
-		unlink := prev.Unlink(1)
-		n := src.Value.(int)
-		dst := prev.Move(n)
-		dst.Link(unlink)
-
 	}
 
-	part1 := zero.Move(1000).Value.(int)
-	part1 += zero.Move(2000).Value.(int)
-	part1 += zero.Move(3000).Value.(int)
+	part21 := zero.Move(1000).Value.(int)
+	part22 := zero.Move(2000).Value.(int)
+	part23 := zero.Move(3000).Value.(int)
 
-	fmt.Println("part1:", part1, "in", time.Since(start))
+	part2 := part21 + part22 + part23
+
+	fmt.Println("part2:", part2, "in", time.Since(start))
 }
