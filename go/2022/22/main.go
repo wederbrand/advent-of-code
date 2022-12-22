d7a09e605cb15f34a7cb44c508cbe7656ae33392
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2022-12-22 23:20:10 +0100

    2022_22 part2 fuck it

--- go/2022/22/main.go
+++ go/2022/22/main.go
@@ -31,16 +31,16 @@ func key(x int, y int) string {
 	return fmt.Sprintf("%d:%d", x, y)
 }
 
-func walk(steps int, x *int, y *int, dx int, dy int, m map[string]*point) {
-	fmt.Println("walking", steps, dx, dy)
+func walk(steps int, x *int, y *int, dx *int, dy *int, m map[string]*point) {
+	//fmt.Println("walking", steps, *dx, *dy)
 	for i := 0; i < steps; i++ {
-		nextX := *x + dx
-		nextY := *y + dy
+		nextX := *x + *dx
+		nextY := *y + *dy
 		p, found := m[key(nextX, nextY)]
 
 		if !found {
 			// out of bounds, warp
-			warp(x, y, &dx, &dy, m)
+			warp(x, y, dx, dy)
 			p = m[key(*x, *y)]
 		}
 
@@ -48,16 +48,17 @@ func walk(steps int, x *int, y *int, dx int, dy int, m map[string]*point) {
 			// stone, walk no more
 			return
 		} else {
+			p.value = "X"
 			// no stone, walk
 			*x = p.x
 			*y = p.y
 		}
 	}
-	fmt.Println(dx, dy)
+	//fmt.Println(*dx, *dy)
 }
 
 // move one step and change x, y, dx and dy
-func warp(x *int, y *int, dx *int, dy *int, m map[string]*point) {
+func warp(x *int, y *int, dx *int, dy *int) {
 	if *x == 99 && *y >= 50 && *y <= 99 && *dx == 1 {
 		// C right -> B bottom
 		*dx = 0
@@ -187,19 +188,6 @@ func warp(x *int, y *int, dx *int, dy *int, m map[string]*point) {
 	fmt.Println("unhandled")
 }
 
-func warpold(x int, y int, dx int, dy int, m map[string]*point) (result *point) {
-	var last *point
-	for found := true; found; last, found = m[key(x, y)] {
-		// reverse direction to warp around
-		// last found is returned
-		x -= dx
-		y -= dy
-		result = last
-	}
-
-	return result
-}
-
 func main() {
 	start := time.Now()
 	readFile, err := os.ReadFile("22/input.txt")
@@ -230,25 +218,24 @@ func main() {
 
 	// follow instructions
 	nbrSteps := 0
-	dx := [4]int{1, 0, -1, 0}
-	dy := [4]int{0, 1, 0, -1}
-	dxyIndex := 0 // right
+	dx := 1
+	dy := 0
+
+	printIt(m)
 
 	for _, r := range instructions {
 		switch r {
 		case 'R':
 			// walk before turning
-			// TODO: need to preserve direction here
-			walk(nbrSteps, &x, &y, dx[dxyIndex], dy[dxyIndex], m)
-			dxyIndex += 1
-			dxyIndex = ((dxyIndex % 4) + 4) % 4
+			walk(nbrSteps, &x, &y, &dx, &dy, m)
+			//printIt(m)
+			right(&dx, &dy)
 			nbrSteps = 0
 		case 'L':
 			// walk before turning
-			// TODO: need to preserve direction here
-			walk(nbrSteps, &x, &y, dx[dxyIndex], dy[dxyIndex], m)
-			dxyIndex -= 1
-			dxyIndex = ((dxyIndex % 4) + 4) % 4
+			walk(nbrSteps, &x, &y, &dx, &dy, m)
+			//printIt(m)
+			left(&dx, &dy)
 			nbrSteps = 0
 		default:
 			// number, append to current number
@@ -257,13 +244,67 @@ func main() {
 		}
 	}
 	// walk if we have more steps to walk
-	walk(nbrSteps, &x, &y, dx[dxyIndex], dy[dxyIndex], m)
+	walk(nbrSteps, &x, &y, &dx, &dy, m)
+	printIt(m)
 
 	part2 := 1000 * (y + 1)
 	part2 += 4 * (x + 1)
-	part2 += dxyIndex % 4
+	switch {
+	case dx == 1 && dy == 0:
+		part2 += 0
+	case dx == 0 && dy == 1:
+		part2 += 1
+	case dx == -1 && dy == 0:
+		part2 += 2
+	case dx == 0 && dy == -1:
+		part2 += 3
+	}
 
-	// not 46324 dxyIndex = 0
-	// not 46325 it's too low
+	// not  46324
+	// not  46325, it's too low
+	// not 126049, it's too low
 	fmt.Println("part2:", part2, "in", time.Since(start))
+}
+
+func printIt(m map[string]*point) {
+	fmt.Println()
+	for y := -1; y < 200; y++ {
+		for x := -1; x < 150; x++ {
+			p, found := m[key(x, y)]
+			if !found {
+				fmt.Print(" ")
+			} else {
+				fmt.Print(p.value)
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func right(dx *int, dy *int) {
+	switch {
+	case *dx == 1 && *dy == 0:
+		*dx, *dy = 0, 1
+	case *dx == 0 && *dy == 1:
+		*dx, *dy = -1, 0
+	case *dx == -1 && *dy == 0:
+		*dx, *dy = 0, -1
+	case *dx == 0 && *dy == -1:
+		*dx, *dy = 1, 0
+	}
+
+}
+
+func left(dx *int, dy *int) {
+	switch {
+	case *dx == 1 && *dy == 0:
+		*dx, *dy = 0, -1
+	case *dx == 0 && *dy == 1:
+		*dx, *dy = 1, 0
+	case *dx == -1 && *dy == 0:
+		*dx, *dy = 0, 1
+	case *dx == 0 && *dy == -1:
+		*dx, *dy = -1, 0
+	}
+
 }
