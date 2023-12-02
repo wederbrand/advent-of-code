eb3e3a5a2f366431fa4e25d977780442c76cee4c
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2023-12-02 08:10:11 +0100

    2023 02 - alternative with regexp

--- github.com/wederbrand/advent-of-code/2023/02/02.go
+++ github.com/wederbrand/advent-of-code/2023/02/02.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/wederbrand/advent-of-code/github.com/wederbrand/advent-of-code/util"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -11,6 +12,7 @@ func main() {
 	start := time.Now()
 	inFile := util.GetFileContents("2023/02/input.txt", "\n")
 
+	colorRe := regexp.MustCompile(`([0-9]+) (red|green|blue)`)
 	part1 := 0
 	part2 := 0
 	for _, s := range inFile {
@@ -21,29 +23,26 @@ func main() {
 		minBlue := 0
 		fmt.Sscanf(s, "Game %d:.*", &gameNbr)
 		games := strings.Split(s, ":")[1]
-		for _, game := range strings.Split(games, ";") {
-			for _, pick := range strings.Split(game, ",") {
-				pick = strings.TrimSpace(pick)
-				cnt := 0
-				color := ""
-				fmt.Sscanf(pick, "%d %s", &cnt, &color)
-				if color == "red" {
-					minRed = util.MaxOf(minRed, cnt)
-					if cnt > 12 {
-						possible = false
-					}
+		all := colorRe.FindAllStringSubmatch(games, -1)
+		for _, matches := range all {
+			color := matches[2]
+			cnt := util.Atoi(matches[1])
+			if color == "red" {
+				minRed = util.MaxOf(minRed, cnt)
+				if cnt > 12 {
+					possible = false
 				}
-				if color == "green" {
-					minGreen = util.MaxOf(minGreen, cnt)
-					if cnt > 13 {
-						possible = false
-					}
+			}
+			if color == "green" {
+				minGreen = util.MaxOf(minGreen, cnt)
+				if cnt > 13 {
+					possible = false
 				}
-				if color == "blue" {
-					minBlue = util.MaxOf(minBlue, cnt)
-					if cnt > 14 {
-						possible = false
-					}
+			}
+			if color == "blue" {
+				minBlue = util.MaxOf(minBlue, cnt)
+				if cnt > 14 {
+					possible = false
 				}
 			}
 		}
