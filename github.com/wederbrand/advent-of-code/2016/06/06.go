7260544d54b8c1e98fa325b66289cb4d3203caef
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2024-11-26 17:20:00 +0100

    2016 06 (warm up)

--- github.com/wederbrand/advent-of-code/2016/06/06.go
+++ github.com/wederbrand/advent-of-code/2016/06/06.go
@@ -3,82 +3,60 @@ package main
 import (
 	"fmt"
 	"github.com/wederbrand/advent-of-code/github.com/wederbrand/advent-of-code/util"
-	"strings"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	inFile := util.GetFileContents("2016/04/input.txt", "\n")
-	part1 := part1(inFile)
+	inFile := util.GetFileContents("2016/06/input.txt", "\n")
+	part1, part2 := doIt(inFile)
 	fmt.Println("Part 1: ", part1, "in", time.Since(start))
-	fmt.Println("Part 2 found manually to be \"northpole object storage\" 984")
+	fmt.Println("Part 2: ", part2, "in", time.Since(start))
 }
 
-func part1(inFile []string) int {
-	sum := 0
+func doIt(inFile []string) (string, string) {
+	letters := make([]map[string]int, len(inFile[0]))
+	for i := range inFile[0] {
+		letters[i] = make(map[string]int)
+	}
+
 	for _, line := range inFile {
-		split := strings.Split(line, "-")
-		words := split[0 : len(split)-1]
-		rest := split[len(split)-1]
-		value := util.Atoi(strings.Split(rest, "[")[0])
-		checksum := rest[len(rest)-6 : len(rest)-1]
-		calculatedSum := getCheckSum(words)
-		if checksum == calculatedSum {
-			sum += value
-			decryptAndPrint(words, value)
+		for i, r := range line {
+			letters[i][string(r)]++
 		}
 	}
 
-	return sum
+	part1 := ""
+	part2 := ""
+
+	for _, m := range letters {
+		part1 += mostCommon(m)
+		part2 += leastCommon(m)
+	}
+
+	return part1, part2
 }
 
-func decryptAndPrint(words []string, value int) {
-	for _, letters := range words {
-		for _, l := range letters {
-			if l == '-' {
-				fmt.Print(" ")
-			} else {
-				shifted := rune((int(l)-'a'+value)%('z'-'a'+1) + 'a')
-				fmt.Printf("%c", shifted)
-			}
+func mostCommon(m map[string]int) string {
+	maxFound := 0
+	maxLetter := ""
+	for letter, count := range m {
+		if count > maxFound {
+			maxFound = count
+			maxLetter = letter
 		}
-		fmt.Print(" ")
 	}
-	fmt.Println(value)
+	return maxLetter
 }
 
-func getCheckSum(words []string) string {
-	result := ""
-
-	counts := make(map[string]int)
-	for _, letters := range words {
-		for _, l := range letters {
-			counts[string(l)]++
+func leastCommon(m map[string]int) string {
+	minFound := 100000
+	minLetter := ""
+	for letter, count := range m {
+		if count < minFound {
+			minFound = count
+			minLetter = letter
 		}
 	}
-
-	for len(result) < 5 {
-		// find the first occurrence of the max count
-		maxFound := 0
-		maxLetter := ""
-		for letter, count := range counts {
-			if count > maxFound || (count == maxFound && letter < maxLetter) {
-				maxFound = count
-				maxLetter = letter
-			}
-		}
-		result += maxLetter
-		counts[maxLetter] = 0 // hide the one we just used
-	}
-
-	return result
-}
-
-func part2(inFile []string) int {
-	valid := 0
-	for i := 0; i < len(inFile); i += 3 {
-	}
-
-	return valid
+	return minLetter
 }
