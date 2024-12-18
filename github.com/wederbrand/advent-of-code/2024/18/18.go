5b5ddb0a67fa5175d4d4593b96747862f523405a
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2024-12-18 08:02:18 +0100

    2024 18 - now also animiated

--- github.com/wederbrand/advent-of-code/2024/18/18.go
+++ github.com/wederbrand/advent-of-code/2024/18/18.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/gdamore/tcell"
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/advent-of-code/util"
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/chart"
 	"github.com/wederbrand/advent-of-code/github.com/wederbrand/priorityqueue"
+	"log"
 	"slices"
+	"sync"
 	"time"
 )
+
+var animation = true
 
 func main() {
 	start := time.Now()
@@ -20,16 +25,41 @@ func main() {
 	minC := Coord{0, 0}
 	maxC := Coord{70, 70}
 
+	tCell, err := tcell.NewScreen()
+	if err != nil {
+		animation = false
+	} else {
+		if err := tCell.Init(); err != nil {
+			log.Fatalf("%+v", err)
+		}
+		// Set default text style
+		defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorMediumSpringGreen)
+		tCell.SetStyle(defStyle)
+
+		// Clear screen
+		tCell.Clear()
+
+		tCellMap(tCell, m, minC, maxC)
+	}
+
 	var currentChart Chart
 	for i, s := range inFile {
 		if i == 1024 {
 			path := walkIt(c, exit, minC, maxC, m)
 
-			fmt.Println("Part 1:", len(path.steps), "in", time.Since(start))
+			if !animation {
+				fmt.Println("Part 1:", len(path.steps), "in", time.Since(start))
+			}
 
 			currentChart = Chart{}
 			for _, step := range path.steps {
 				currentChart[step] = "O"
+				if animation {
+					tCellPath(tCell, step, false)
+				}
+			}
+			if animation {
+				tCell.Show()
 			}
 		}
 		var x, y int
@@ -37,20 +67,48 @@ func main() {
 		nextByte := Coord{x, y}
 		m[nextByte] = "#"
 
+		if animation {
+			tCellByte(tCell, nextByte, false)
+		}
+
 		if i > 1024 {
 			if currentChart[nextByte] == "O" {
 				// This is where we used to walk, take a new route
 				path := walkIt(c, exit, minC, maxC, m)
 
 				if len(path.steps) == 0 {
-					fmt.Println("Part 2:", s, "in", time.Since(start))
+					if !animation {
+						fmt.Println("Part 2:", s, "in", time.Since(start))
+					} else {
+						tCellByte(tCell, nextByte, true)
+						wg := sync.WaitGroup{}
+						wg.Add(1)
+						go func() {
+							time.Sleep(5 * time.Second)
+							wg.Done()
+						}()
+						wg.Wait()
+					}
+
 					break
 				}
 
+				if animation {
+					for coord := range currentChart {
+						tCellPath(tCell, coord, true)
+					}
+				}
 				currentChart = Chart{}
 				for _, step := range path.steps {
 					currentChart[step] = "O"
 				}
+				if animation {
+					for coord := range currentChart {
+						tCellPath(tCell, coord, false)
+					}
+					tCell.Show()
+				}
+
 			}
 		}
 	}
@@ -91,4 +149,38 @@ func walkIt(start Coord, exit Coord, minC Coord, maxC Coord, m Chart) Path {
 	}
 
 	return Path{[]Coord{}}
+}
+
+func tCellMap(cell tcell.Screen, m Chart, minC Coord, maxC Coord) {
+	for y := minC.Y; y <= maxC.Y; y++ {
+		for x := minC.X; x <= maxC.X; x++ {
+			s, found := m[Coord{x, y}]
+			if !found {
+				s = "."
+			}
+			cell.SetContent(x, y, rune(s[0]), nil, tcell.StyleDefault)
+		}
+	}
+
+	cell.Show()
+	//time.Sleep(1 * time.Millisecond)
+}
+
+func tCellByte(cell tcell.Screen, byte Coord, final bool) {
+	byteStyle := tcell.StyleDefault
+	if final {
+		byteStyle = tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorDarkGoldenrod).Blink(true)
+	}
+	cell.SetContent(byte.X, byte.Y, '#', nil, byteStyle)
+
+	cell.Show()
+	time.Sleep(3 * time.Millisecond)
+}
+
+func tCellPath(cell tcell.Screen, path Coord, old bool) {
+	pathStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorOrangeRed)
+	if old {
+		pathStyle = tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorPink)
+	}
+	cell.SetContent(path.X, path.Y, '0', nil, pathStyle)
 }
