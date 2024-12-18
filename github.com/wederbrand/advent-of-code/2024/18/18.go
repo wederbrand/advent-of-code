e0556e4520465a984a9b756a1d12d1f0af86add5
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2024-12-18 07:29:34 +0100

    2024 18 - much faster but much messier
    Instead of just drawing it from scratch I now keep track of where I
    walked the last time and let bytes fall freely until that last path is
    broken. Then I calculate a new path.

--- github.com/wederbrand/advent-of-code/2024/18/18.go
+++ github.com/wederbrand/advent-of-code/2024/18/18.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/advent-of-code/util"
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/chart"
 	"github.com/wederbrand/advent-of-code/github.com/wederbrand/priorityqueue"
+	"slices"
 	"time"
 )
 
@@ -19,37 +20,58 @@ func main() {
 	minC := Coord{0, 0}
 	maxC := Coord{70, 70}
 
+	var currentChart Chart
 	for i, s := range inFile {
 		if i == 1024 {
-			steps := walkIt(c, exit, minC, maxC, m)
+			path := walkIt(c, exit, minC, maxC, m)
 
-			fmt.Println("Part 1:", steps, "in", time.Since(start))
+			fmt.Println("Part 1:", len(path.steps), "in", time.Since(start))
+
+			currentChart = Chart{}
+			for _, step := range path.steps {
+				currentChart[step] = "O"
+			}
 		}
 		var x, y int
 		fmt.Sscanf(s, "%d,%d", &x, &y)
-		m[Coord{x, y}] = "#"
+		nextByte := Coord{x, y}
+		m[nextByte] = "#"
 
 		if i > 1024 {
-			steps := walkIt(c, exit, minC, maxC, m)
+			if currentChart[nextByte] == "O" {
+				// This is where we used to walk, take a new route
+				path := walkIt(c, exit, minC, maxC, m)
 
-			if steps == -1 {
-				fmt.Println("Part 2:", s, "in", time.Since(start))
-				break
+				if len(path.steps) == 0 {
+					fmt.Println("Part 2:", s, "in", time.Since(start))
+					break
+				}
+
+				currentChart = Chart{}
+				for _, step := range path.steps {
+					currentChart[step] = "O"
+				}
 			}
 		}
 	}
 }
 
-func walkIt(start Coord, exit Coord, minC Coord, maxC Coord, m Chart) int {
+type Path struct {
+	steps []Coord
+}
+
+func walkIt(start Coord, exit Coord, minC Coord, maxC Coord, m Chart) Path {
 	q := priorityqueue.NewQueue()
-	q.Add(&priorityqueue.State{Data: start, Priority: 0})
+	q.Add(&priorityqueue.State{Data: Path{[]Coord{start}}, Priority: 0})
 
 	seen := make(map[Coord]bool)
 	for q.HasNext() {
 		s := q.Next()
-		c := s.Data.(Coord)
+		p := s.Data.(Path)
+		c := p.steps[len(p.steps)-1]
+
 		if c == exit {
-			return s.Priority
+			return p
 		}
 
 		if seen[c] {
@@ -62,9 +84,11 @@ func walkIt(start Coord, exit Coord, minC Coord, maxC Coord, m Chart) int {
 			if m[next] == "#" || next.X < minC.X || next.Y < minC.Y || next.X > maxC.X || next.Y > maxC.Y {
 				continue
 			}
-			q.Add(&priorityqueue.State{Data: next, Priority: s.Priority + 1})
+			newPath := Path{slices.Clone(p.steps)}
+			newPath.steps = append(newPath.steps, next)
+			q.Add(&priorityqueue.State{Data: newPath, Priority: s.Priority + 1})
 		}
 	}
 
-	return -1
+	return Path{[]Coord{}}
 }
