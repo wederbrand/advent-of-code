773f37c59fb5f4e2fda5991c9192b5d4ff51f3ef
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2024-12-27 19:20:50 +0100

    2016 15
    I know it's fully possible to just do the the mod-way but this is how it
    ended up after part 1 and seems it works for part 2 in ~50ms so...

--- github.com/wederbrand/advent-of-code/2016/15/15.go
+++ github.com/wederbrand/advent-of-code/2016/15/15.go
@@ -3,61 +3,65 @@ package main
 import (
 	"fmt"
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/advent-of-code/util"
-	"strconv"
-	"strings"
 	"time"
 )
 
-func main() {
-	start := time.Now()
-	inFile := GetFileContents("2016/12/input.txt", "\n")
-
-	instructions := make([]string, 0)
-	for _, line := range inFile {
-		instructions = append(instructions, line)
-	}
-
-	registers := make(map[string]int)
-	doIt(instructions, registers)
-	fmt.Println("Part 1: ", registers["a"], "in", time.Since(start))
-
-	registers = make(map[string]int)
-	registers["c"] = 1
-	doIt(instructions, registers)
-	fmt.Println("Part 2: ", registers["a"], "in", time.Since(start))
+type Disc struct {
+	positions int
+	current   int
 }
 
-func doIt(instructions []string, registers map[string]int) {
-	pointer := 0
-	for pointer < len(instructions) {
-		instruction := instructions[pointer]
-		split := strings.Split(instruction, " ")
-		switch split[0] {
-		case "cpy":
-			value, err := strconv.Atoi(split[1])
-			if err != nil {
-				value = registers[split[1]]
-			}
-			registers[split[2]] = value
-			pointer++
-		case "inc":
-			registers[split[1]]++
-			pointer++
-		case "dec":
-			registers[split[1]]--
-			pointer++
-		case "jnz":
-			value, err := strconv.Atoi(split[1])
-			if err != nil {
-				value = registers[split[1]]
-			}
-			if value != 0 {
-				pointer += Atoi(split[2])
-			} else {
-				pointer++
-			}
-		default:
-			panic("Unknown instruction")
+func (d *Disc) rotate(steps int) {
+	newCurrent := (d.current + steps) % d.positions
+	d.current = newCurrent
+}
+
+func main() {
+	start := time.Now()
+	inFile := GetFileContents("2016/15/input.txt", "\n")
+
+	part1 := doIt(inFile)
+	fmt.Println("Part 1: ", part1, "in", time.Since(start))
+
+	part2 := doIt(append(inFile, "Disc #7 has 11 positions; at time=0, it is at position 0."))
+	fmt.Println("Part 2: ", part2, "in", time.Since(start))
+}
+
+func doIt(inFile []string) int {
+	discs := make([]*Disc, len(inFile))
+
+	for _, line := range inFile {
+		var d Disc
+		var i int
+		_, err := fmt.Sscanf(line, "Disc #%d has %d positions; at time=0, it is at position %d.", &i, &d.positions, &d.current)
+		if err != nil {
+			panic(err)
 		}
+		discs[i-1] = &d
 	}
+
+	// move disc start position according to time
+	for i, d := range discs {
+		d.rotate(i + 1)
+	}
+
+	// find the first time when all discs are at 0
+	dropTime := 0
+	found := false
+	for !found {
+		for _, d := range discs {
+			d.rotate(1)
+		}
+
+		found = true
+		for _, d := range discs {
+			if d.current != 0 {
+				found = false
+				break
+			}
+		}
+
+		dropTime++
+	}
+	return dropTime
 }
