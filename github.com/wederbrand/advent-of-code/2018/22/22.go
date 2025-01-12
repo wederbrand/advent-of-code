af52a4a04e4f270f6be27552c1c74d8ed9be24b8
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2025-01-12 17:08:40 +0100

    2018 22

--- github.com/wederbrand/advent-of-code/2018/22/22.go
+++ github.com/wederbrand/advent-of-code/2018/22/22.go
@@ -4,125 +4,157 @@ import (
 	"fmt"
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/advent-of-code/util"
 	. "github.com/wederbrand/advent-of-code/github.com/wederbrand/chart"
-	"strconv"
-	"strings"
+	"github.com/wederbrand/advent-of-code/github.com/wederbrand/priorityqueue"
 	"time"
 )
 
+type cave struct {
+	geoIndex int
+	erosion  int
+	region   string
+}
+
 func main() {
 	start := time.Now()
-	inFile := GetFileContents("2018/20/input.txt", "\n")
+	inFile := GetFileContents("2018/22/input.txt", "\n")
 
-	m := Chart{}
+	depth := 0
+	fmt.Sscanf(inFile[0], "depth: %d", &depth)
+	target := Coord{0, 0}
+	fmt.Sscanf(inFile[1], "target: %d,%d", &target.X, &target.Y)
 
-	c := Coord{0, 0}
-	m[c] = "X"
+	m := map[Coord]cave{}
+	entrance := Coord{0, 0}
 
-	split := strings.Split(inFile[0], "")
-	split = split[1 : len(split)-1]
+	part1 := 0
+	for y := 0; y <= target.Y; y++ {
+		for x := 0; x <= target.X; x++ {
+			c := Coord{x, y}
+			caveRoom := getRoom(c, m, depth, entrance, target)
+			m[c] = caveRoom
+			switch caveRoom.region {
+			case ".":
+				part1 += 0
+			case "=":
+				part1 += 1
+			case "|":
+				part1 += 2
+			}
+		}
+	}
 
-	createMap(m, c, strings.Join(split, ""))
-	fmt.Println("Map created in", time.Since(start))
-	
-	newM := CopyChart(m)
-	part1 := floodFill(newM, c, 0)
 	fmt.Println("Part 1:", part1, "in", time.Since(start))
 
-	part2 := 0
-	for _, v := range newM {
-		atoi, err := strconv.Atoi(v)
-		if err == nil && atoi >= 1000 {
-			part2++
-		}
-	}
-
+	part2 := findTarget(m, depth, entrance, target)
 	fmt.Println("Part 2:", part2, "in", time.Since(start))
-
 }
 
-func floodFill(m Chart, c Coord, i int) int {
-	// mark this as 0, don't mark the X
-	// walk in all directions with doors recursively
-	// if we hit a number stop walking
+type CaveState struct {
+	coord Coord
+	tool  string
+}
 
-	if m[c] != "." && m[c] != "X" {
-		return i - 1
-	}
+func findTarget(m map[Coord]cave, depth int, entrance Coord, target Coord) int {
+	q := priorityqueue.NewQueue()
+	q.Add(&priorityqueue.State{Data: CaveState{entrance, "torch"}, Priority: 0})
 
-	if m[c] != "X" {
-		m[c] = fmt.Sprintf("%d", i)
-	}
+	seen := map[CaveState]int{}
+	for q.HasNext() {
+		s := q.Next()
+		cs := s.Data.(CaveState)
+		c := cs.coord
+		currentRoom := m[c]
+		t := cs.tool
 
-	maxVal := 0
-	for _, d := range ALL {
-		if m[c.Move(d)] == "-" || m[c.Move(d)] == "|" {
-			v := floodFill(m, c.Move(d, 2), i+1)
-			if v > maxVal {
-				maxVal = v
+		if c == target && t == "torch" {
+			return s.Priority
+		}
+
+		if val, ok := seen[cs]; ok && val <= s.Priority {
+			continue
+		}
+		seen[cs] = s.Priority
+
+		// for all directions, and change tool if needed
+		for _, d := range ALL {
+			next := c.Move(d)
+			if next.X < 0 || next.Y < 0 {
+				continue
+			}
+
+			if _, ok := m[next]; !ok {
+				m[next] = getRoom(next, m, depth, entrance, target)
+			}
+
+			nextRoom := m[next]
+
+			if nextRoom.region == "." {
+				// needs climbing or torch
+				if t == "torch" || t == "climbing" {
+					q.Add(&priorityqueue.State{Data: CaveState{next, t}, Priority: s.Priority + 1})
+				}
+			} else if nextRoom.region == "=" {
+				// needs climbing or neither
+				if t == "climbing" || t == "neither" {
+					q.Add(&priorityqueue.State{Data: CaveState{next, t}, Priority: s.Priority + 1})
+				}
+			} else if nextRoom.region == "|" {
+				// needs torch or neither
+				if t == "torch" || t == "neither" {
+					q.Add(&priorityqueue.State{Data: CaveState{next, t}, Priority: s.Priority + 1})
+				}
+			}
+		}
+
+		// stay and change tool
+		if currentRoom.region == "." {
+			if t == "torch" {
+				q.Add(&priorityqueue.State{Data: CaveState{c, "climbing"}, Priority: s.Priority + 7})
+			} else if t == "climbing" {
+				q.Add(&priorityqueue.State{Data: CaveState{c, "torch"}, Priority: s.Priority + 7})
+			}
+		} else if currentRoom.region == "=" {
+			if t == "climbing" {
+				q.Add(&priorityqueue.State{Data: CaveState{c, "neither"}, Priority: s.Priority + 7})
+			} else if t == "neither" {
+				q.Add(&priorityqueue.State{Data: CaveState{c, "climbing"}, Priority: s.Priority + 7})
+			}
+		} else if currentRoom.region == "|" {
+			if t == "torch" {
+				q.Add(&priorityqueue.State{Data: CaveState{c, "neither"}, Priority: s.Priority + 7})
+			} else if t == "neither" {
+				q.Add(&priorityqueue.State{Data: CaveState{c, "torch"}, Priority: s.Priority + 7})
 			}
 		}
 	}
-	return maxVal
-
+	panic("No path found")
 }
 
-type cacheKey struct {
-	c  Coord
-	in string
-}
-
-var cache = make(map[cacheKey]bool)
-
-func createMap(m Chart, c Coord, in string) {
-	key := cacheKey{c, in}
-	if _, ok := cache[key]; ok {
-		return
-	}
-	cache[key] = true
-
-	if len(in) == 0 {
-		return
-	}
-	if in[0] == '(' {
-		inputs := make([]string, 0)
-		input := ""
-		i := 0
-		open := 1
-		for i = 1; i < len(in); i++ {
-			if in[i] == '|' {
-				if open == 1 {
-					inputs = append(inputs, input)
-					input = ""
-				} else {
-					input += string(in[i])
-				}
-			} else if in[i] == ')' {
-				open--
-				if open == 0 {
-					inputs = append(inputs, input)
-					break
-				} else {
-					input += string(in[i])
-				}
-			} else if in[i] == '(' {
-				open++
-				input += string(in[i])
-			} else {
-				input += string(in[i])
-			}
-		}
-		for _, j := range inputs {
-			createMap(m, c, j+in[i+1:])
-		}
+func getRoom(c Coord, m map[Coord]cave, depth int, entrance Coord, target Coord) cave {
+	geoIndex := 0
+	if c == entrance || c == target {
+		geoIndex = 0
+	} else if c.Y == 0 {
+		geoIndex = c.X * 16807
+	} else if c.X == 0 {
+		geoIndex = c.Y * 48271
 	} else {
-		d := FromCompassChar(in[0])
-		if d == N || d == S {
-			m[c.Move(d)] = "-"
-		} else {
-			m[c.Move(d)] = "|"
+		if _, ok := m[c.Move(LEFT)]; !ok {
+			m[c.Move(LEFT)] = getRoom(c.Move(LEFT), m, depth, entrance, target)
 		}
-		c = c.Move(d, 2)
-		m[c] = "."
-		createMap(m, c, in[1:])
+		if _, ok := m[c.Move(UP)]; !ok {
+			m[c.Move(UP)] = getRoom(c.Move(UP), m, depth, entrance, target)
+		}
+		geoIndex = m[c.Move(LEFT)].erosion * m[c.Move(UP)].erosion
 	}
+	erosion := (geoIndex + depth) % 20183
+	switch erosion % 3 {
+	case 0:
+		return cave{geoIndex, erosion, "."}
+	case 1:
+		return cave{geoIndex, erosion, "="}
+	case 2:
+		return cave{geoIndex, erosion, "|"}
+	}
+	panic("Unknown region")
 }
